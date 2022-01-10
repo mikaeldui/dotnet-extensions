93d9fe3b4b73dec66b8be75a66da2d3f04755712
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-10 13:50:25 +0100

    Added more extension methods

---
+++ DotNet.Extensions/Net/Http/HttpClientExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Net.Http
+{
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Send a GET request to the specified Uri with a cancellation token, and serialize the HTTP content to a string, as an asynchronous operation
+        /// </summary>
+        public static async Task<string> GetStringAsync(this HttpClient httpClient, Uri requestUri, CancellationToken cancellationToken)
+        {
+            var response = await httpClient.GetAsync(requestUri, cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Send a GET request to the specified Uri with a cancellation token, and serialize the HTTP content to a string, as an asynchronous operation
+        /// </summary>
+        public static async Task<string> GetStringAsync(this HttpClient httpClient, string requestUri, CancellationToken cancellationToken)
+        {
+            var response = await httpClient.GetAsync(requestUri, cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
