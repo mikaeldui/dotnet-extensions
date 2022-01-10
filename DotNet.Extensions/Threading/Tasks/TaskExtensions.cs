69e79f3089d541de9f283bb9b513373bb6c91ac6
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-10 14:10:31 +0100

    Added Task.TimeoutAfter()

---
+++ DotNet.Extensions/Threading/Tasks/TaskExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Threading.Tasks
+{
+    public static class TaskExtensions
+    {
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;  // Very important in order to propagate exceptions
+                }
+                else
+                {
+                    throw new TimeoutException("The operation has timed out.");
+                }
+            }
+        }
+
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, int timeoutInMilliseconds) => await task.TimeoutAfter(TimeSpan.FromMilliseconds(timeoutInMilliseconds));
+    }
+}
