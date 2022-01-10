7e1611b36214bca3b091e3ec1c8019fa25b3c3b3
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-10 13:14:59 +0100

    Added Post- and PutAsJsonAsync<TValue, TResult>

---
+++ DotNet.Extensions/Net/Http/Json/HttpClientJsonExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Net.Http.Json
+{
+    internal static class HttpClientJsonExtensions
+    {
+        public static async Task<TResult?> PostAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value)
+        {
+            var response = await httpClient.PostAsJsonAsync(requestUri, value);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResult>();
+        }
+
+        public static async Task<TResult?> PutAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value)
+        {
+            var response = await httpClient.PutAsJsonAsync(requestUri, value);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResult>();
+        }
+    }
+}
