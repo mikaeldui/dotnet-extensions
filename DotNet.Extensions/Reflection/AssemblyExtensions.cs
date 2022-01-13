045d8ccc0858c8066143efb950625e87531bd845
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-13 05:57:28 +0100

    Added AssemblyExtensions

---
+++ DotNet.Extensions/Reflection/AssemblyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace System.Reflection
+{
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Like "1.2.3.0".
+        /// </summary>
+        public static string GetFileVersion(this Assembly assembly) =>
+            assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+
+        /// <summary>
+        /// Like "1.2.3+1a2b3c4d".
+        /// </summary>
+        public static string GetProductVersion(this Assembly assembly) =>
+            FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+    }
+}
