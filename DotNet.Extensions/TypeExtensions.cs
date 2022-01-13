fef632b74441b245771e714e41a7172c6ad16c8b
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-13 11:23:47 +0100

    Added IsSubTypeOfRawGeneric.

---
+++ DotNet.Extensions/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Reflection
+{
+    public static class TypeExtensions
+    {
+        public static bool IsSubTypeOfRawGeneric(this Type generic, Type toCheck)
+        {
+            while (toCheck != null && toCheck != typeof(object))
+            {
+                var cur = toCheck.IsGenericType ? toCheck.GetGenericTypeDefinition() : toCheck;
+                if (generic == cur)
+                {
+                    return true;
+                }
+                toCheck = toCheck.BaseType;
+            }
+            return false;
+        }
+    }
+}
