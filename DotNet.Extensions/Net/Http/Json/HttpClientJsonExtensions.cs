7f29ea953b004c37d984bcabcba9034542d07cb9
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-19 04:46:10 +0100

    Added CancellationToken.

--- DotNet.Extensions/Net/Http/Json/HttpClientJsonExtensions.cs
+++ DotNet.Extensions/Net/Http/Json/HttpClientJsonExtensions.cs
@@ -6,18 +6,18 @@ namespace System.Net.Http.Json
 {
     public static class HttpClientJsonExtensions
     {
-        public static async Task<TResult?> PostAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value)
+        public static async Task<TResult?> PostAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value, CancellationToken cancellationToken = default)
         {
-            var response = await httpClient.PostAsJsonAsync(requestUri, value);
+            var response = await httpClient.PostAsJsonAsync(requestUri, value, cancellationToken);
             response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<TResult>();
+            return await response.Content.ReadFromJsonAsync<TResult>(cancellationToken: cancellationToken);
         }
 
-        public static async Task<TResult?> PutAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value)
+        public static async Task<TResult?> PutAsJsonAsync<TValue, TResult>(this HttpClient httpClient, string? requestUri, TValue value, CancellationToken cancellationToken = default)
         {
-            var response = await httpClient.PutAsJsonAsync(requestUri, value);
+            var response = await httpClient.PutAsJsonAsync(requestUri, value, cancellationToken);
             response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<TResult>();
+            return await response.Content.ReadFromJsonAsync<TResult>(cancellationToken: cancellationToken);
         }
     }
 }
