2805f3871e5280bc5390f0a2c84cf6c544b598eb
Mikael Dúi Bolinder <github@bolinder.uk>
2022-02-05 04:38:02 +0100

    Better null handling in AssemblyExtensions.

--- DotNet.Extensions/Reflection/AssemblyExtensions.cs
+++ DotNet.Extensions/Reflection/AssemblyExtensions.cs
@@ -10,19 +10,19 @@ namespace System.Reflection
         /// <summary>
         /// Like "1.2.3.0".
         /// </summary>
-        public static string GetFileVersion(this Assembly assembly) =>
-            assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+        public static string? GetFileVersion(this Assembly assembly) =>
+            assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version;
 
         /// <summary>
         /// Like "1.2.3+1a2b3c4d".
         /// </summary>
-        public static string GetInformationalVersion(this Assembly assembly) =>
-            assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.ToString();
+        public static string? GetInformationalVersion(this Assembly assembly) =>
+            assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion?.ToString();
 
         /// <summary>
         /// Like "1.2.3+1a2b3c4d". Doesn't work with WebAssembly! Use <see cref="GetInformationalVersion(Assembly)"/> instead.
         /// </summary>
-        public static string GetProductVersion(this Assembly assembly) =>
-            FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+        public static string? GetProductVersion(this Assembly assembly) =>
+            FileVersionInfo.GetVersionInfo(assembly.Location)?.ProductVersion;
     }
 }
